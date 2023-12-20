13addf670a124766417344836bcf2d55cc6db36a
Hanan <h.ur-rehman@wafios.de>
2023-12-20 17:39:35 +0100

    Practice tests with Parabank

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,20 @@
 import { test, expect } from "@playwright/test";
+test.describe("Parabank homepage", () => {
+  test("Check login form title", async ({ page }) => {
+    await page.goto("https://parabank.parasoft.com/parabank/index.htm");
+    const title = page.locator("#leftPanel > h2");
+    await page.pause();
+    await expect.soft(title).toHaveText("Customer Login");
+    await expect(page.locator("#topPanel > p")).toHaveText(
+      "Experience the difference"
+    );
+  });
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  test("Check username label", async ({ page, browserName }) => {
+    test.fixme(browserName === "chromium", "Fix this test");
+    await page.goto("https://parabank.parasoft.com/parabank/index.htm");
+    const text = page.locator("#loginPanel > form > p:nth-child(1) > b");
+    await expect(text).toHaveText("Username");
+  });
 });
+//TODO: Watch tutorials for fixtures and extending them
