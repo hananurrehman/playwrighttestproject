bd0df20aadaf018a2916538f43c53d0f0046cafd
Hanan <h.ur-rehman@wafios.de>
2024-01-01 17:23:38 +0100

    Expanded test heroku app POM and fixtures

--- tests/contact.page.ts
+++ tests/contact.page.ts
@@ -6,12 +6,45 @@ import type { Page, Locator } from "@playwright/test";
 export default class ContactPage {
   page: Page;
 
+  //Contacts list
   readonly pageTitle: Locator;
   readonly pageDescription: Locator;
+  readonly addContactBtn: Locator;
+
+  //Add contact form
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly dobInput: Locator;
+  readonly phoneNumberInput: Locator;
+  readonly emailInput: Locator;
+  readonly streetAddressOneInput: Locator;
+  readonly streetAddressTwoInput: Locator;
+  readonly cityInput: Locator;
+  readonly stateInput: Locator;
+  readonly postalCodeInput: Locator;
+  readonly countryInput: Locator;
+  readonly submitBtn: Locator;
 
   constructor(page: Page) {
     this.page = page;
+
+    //Contacts list
     this.pageTitle = page.locator("header > h1");
     this.pageDescription = page.locator("header + p");
+    this.addContactBtn = page.locator("#add-contact");
+
+    //Add contact form
+    this.firstNameInput = page.locator("#firstName");
+    this.lastNameInput = page.locator("#lastName");
+    this.dobInput = page.locator("#birthdate");
+    this.emailInput = page.locator("#email");
+    this.phoneNumberInput = page.locator("#phone");
+    this.streetAddressOneInput = page.locator("#street1");
+    this.streetAddressTwoInput = page.locator("#street2");
+    this.cityInput = page.locator("#city");
+    this.stateInput = page.locator("#stateProvince");
+    this.postalCodeInput = page.locator("#postalCode");
+    this.countryInput = page.locator("#country");
+    this.submitBtn = page.locator("#submit");
   }
 }
--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -10,4 +10,36 @@ test1(
     console.log("Checked contact page title and description");
   }
 );
-//TODO: Add contacts and test
+
+test1(
+  "Add contact and check successfully added",
+  async ({ homePage, contactPage, page }) => {
+    //Add a contact
+    contactPage.addContactBtn.click();
+    const currentDate = new Date().toISOString().slice(0, 10);
+    await contactPage.firstNameInput.fill("Test");
+    await contactPage.lastNameInput.fill("Contact");
+    await contactPage.dobInput.fill(currentDate);
+    await contactPage.emailInput.fill("test@hanan.com");
+    await contactPage.phoneNumberInput.fill("123456789");
+    await contactPage.streetAddressOneInput.fill("P Sherman 42");
+    await contactPage.streetAddressTwoInput.fill("Wallaby Way");
+    await contactPage.cityInput.fill("Sydney");
+    await contactPage.stateInput.fill("Sydney");
+    await contactPage.postalCodeInput.fill("123456");
+    await contactPage.countryInput.fill("Austrialia");
+    await contactPage.submitBtn.click();
+
+    //Check contact is added on the table view
+    //This approach picks the 1st table row and therefore would fail if it did not find the required text
+    //We have a soft assertion here to keep the test going
+    await expect
+      .soft(page.locator(".contactTableBodyRow > td").nth(1))
+      .toHaveText("Test Contact", { timeout: 3000 });
+    //This approach picks which ever table row that contains the required text
+    await expect(
+      page.locator(".contactTableBodyRow > td").getByText("Test Contact")
+    ).toHaveCount(1);
+  }
+);
+//TODO: Add deletion case
